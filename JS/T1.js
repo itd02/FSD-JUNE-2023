c1655e2775d939189568d065bbd3061d3d481057
itdefined <117959060+itdefined@users.noreply.github.com>
2023-06-27 09:37:50 +0530

    updated

---
+++ JS/T1.js
@@ -0,0 +1,36 @@
+/*
+             JS 1 Tutorial
+-------------------------------------------------
+
+1. Primitive Data Types
+   a. Numbers
+   b. Strings
+   c. Boolean
+   d. Undefined
+   e. Null
+   f. Symbols
+
+2. Non-Primitive Data Types
+   a. Objects
+   b. Functions
+   c. Arrays
+
+*/
+
+
+let age = 10
+let height = -20
+let z = -23.97
+let country = `The country name is "India"`
+let passed = false
+let something
+let somethingElse = null
+
+console.log('The type of height is ==> ', typeof(height))
+console.log('The type of country is ==> ', typeof(country))
+console.log('The type of passed is ==> ', typeof(passed))
+console.log('The type of something is ==> ', typeof(something))
+console.log('The type of somethingElse is ==> ', typeof(somethingElse))
+
+let <identifier>
+let <identifier> = <value>
---
+++ JS/T2.js
@@ -0,0 +1,71 @@
+/* Numbers */
+
+let age = 20
+let height = 5.7
+const PI = 3.142
+
+// console.log(Math.round(PI))
+// console.log(Math.round(height))
+// console.log(Math.ceil(PI))
+// console.log(Math.pow(4, 3))
+
+
+/* Strings */
+
+let blank = ' '
+let firstName = 'Virat'
+let lastName = 'Kohli'
+
+
+let bioData = `Virat Kohli (Hindi pronunciation: 
+	born 5 November 1988) is an Indian international 
+    cricketer and the former captain of the Indian national 
+    cricket team who plays as a right-handed batsman for 
+    Royal Challengers Bangalore in the IPL and for Delhi 
+    in Indian domestic cricket. Widely regarded as one of 
+    the greatest batsmen of all time,[4] Kohli holds the 
+    records for scoring most runs in T20 internationals 
+    and in the IPL. In 2020, the International Cricket 
+    Council named him the male cricketer of the decade. 
+    Kohli has also contributed to India's 
+    successes, including winning the 2011 
+    World Cup and the 2013 Champions trophy.`
+
+
+let fullName = firstName + ' ' + lastName
+// console.log(firstName)
+// console.log(lastName)
+// console.log(fullName)
+
+let something = 'Virat Kohli (\'Hindi\' pronunciation: born 5 November 1988) is an Indian international '
+
+
+//console.log(something)
+
+
+let num1 = 200
+let num2 = 100
+//console.log("The sum of "+ num1 + " and "+ num2 + " is  "+ sum)
+//console.log(`The sum of ${num1} and ${num2} is ${num1 + num2}`)
+
+
+//console.log(lastName)
+//console.log(lastName[lastName.length-2])
+//console.log(lastName.length)
+
+//console.log(lastName.toUpperCase())
+//console.log(bioData.length)
+//console.log(bioData.substr(663,17))
+//console.log(lastName.length)
+//console.log(lastName[5])
+
+//console.log(lastName)
+//console.log(lastName.substring(2, 3))
+
+
+let team = "Royal Challengers Bangalore"
+//console.log(team.split('Challengers'))
+let someText = "          Challengers            "
+//console.log(someText.trim().length)
+
+console.log(bioData.includes('runs'))
